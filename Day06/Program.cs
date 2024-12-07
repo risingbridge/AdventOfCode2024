8169026ae63088f64fca199eb0a312968497b2f1
Håvard <36506751+risingbridge@users.noreply.github.com>
2024-12-07 21:42:58 +0100

    Solved Day06. Worst bruteforce solution.

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -4,6 +4,7 @@ List<string> input = File.ReadAllLines("input.txt").ToList();
 char[,] map = new char[input.Count, input[0].Length];
 Vector2 playerPosition = new Vector2(0, 0);
 FacingDirection playerDir = FacingDirection.Up;
+FacingDirection defaultDir = FacingDirection.Up;
 for (int y = 0; y < input.Count; y++)
 {
     for (int x = 0; x < input[0].Length; x++)
@@ -16,9 +17,10 @@ for (int y = 0; y < input.Count; y++)
         }
     }
 }
-
+Vector2 playerDefaultPos = new Vector2(playerPosition.X, playerPosition.Y);
 List<Vector2> distinctLocations = new List<Vector2>();
 distinctLocations.Add(playerPosition);
+int stepCounter = 0;
 //Loop forever
 while (true)
 {
@@ -28,7 +30,7 @@ while (true)
     Vector2 newPos = MoveForward(playerDir, playerPosition);
     if (newPos.X < 0 || newPos.Y < 0 || newPos.X >= map.GetLength(1) || newPos.Y >= map.GetLength(0))
     {
-        Console.WriteLine("Out of bounds!");
+        //Console.WriteLine("Out of bounds!");
         break;
     }
 
@@ -39,6 +41,7 @@ while (true)
     else
     {
         playerPosition = newPos;
+        stepCounter++;
     }
 
     if (!distinctLocations.Contains(playerPosition))
@@ -46,14 +49,76 @@ while (true)
         distinctLocations.Add(playerPosition);
     }
 
-    Console.Clear();
+    //Console.Clear();
     //PrintMap(map);
-    //Thread.Sleep(500);
+    //Thread.Sleep(10);
 }
 Console.WriteLine($"Part One: {distinctLocations.Count}");
+//Console.WriteLine($"Used number of steps: {stepCounter}");
+
+//Solve part two
+int loopCount = 0;
+for (int y = 0; y < map.GetLength(0); y++)
+{
+    for (int x = 0; x < map.GetLength(1); x++)
+    {
+        char c = map[y, x];
+        if (c == '.')
+        {
+            char[,] workingMap = map.Clone() as char[,];
+            playerPosition = new Vector2(playerDefaultPos.X, playerDefaultPos.Y);
+            playerDir = defaultDir;
+            //Try to place an obstacle
+            workingMap[y, x] = '#';
+            int currentStepCounter = 0;
+            while (true)
+            {
+                //Move player forward
+                //If it hits #, turn right
+                //Continue
+                Vector2 newPos = MoveForward(playerDir, playerPosition);
+                if (newPos.X < 0 || newPos.Y < 0 || newPos.X >= workingMap.GetLength(1) || newPos.Y >= workingMap.GetLength(0))
+                {
+                    //Console.WriteLine("Out of bounds!");
+                    //PrintMap(workingMap, new Vector2(x, y));
+                    //Console.WriteLine($"Steps: {currentStepCounter}");
+                    break;
+                }
+
+                if (workingMap[(int)newPos.Y, (int)newPos.X] == '#')
+                {
+                    playerDir = TurnRightNighty(playerDir);
+                }
+                else
+                {
+                    playerPosition = newPos;
+                    currentStepCounter++;
+                }
+
+                if (!distinctLocations.Contains(playerPosition))
+                {
+                    distinctLocations.Add(playerPosition);
+                }
+
+                //Console.Clear();
+                //PrintMap(map);
+                //Thread.Sleep(10);
+                if (currentStepCounter >= (stepCounter * 10))
+                {
+                    //Console.WriteLine("This loops!");
+                    loopCount++;
+                    break;
+                }
+            }
+
+            //Console.WriteLine();
+        }
+    }
+}
+Console.WriteLine($"Part Two: {loopCount}");
 
 //Function to visualize map
-void PrintMap(char[,] map)
+void PrintMap(char[,] map, Vector2? newObstalce)
 {
     for (int y = 0; y < map.GetLength(0); y++)
     {
@@ -84,7 +149,17 @@ void PrintMap(char[,] map)
             }
             else
             {
-                Console.Write(map[y, x]);
+                if (new Vector2(x, y) == newObstalce)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write('O');
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write(map[y, x]);
+                }
+                
             }
         }
         Console.WriteLine();
